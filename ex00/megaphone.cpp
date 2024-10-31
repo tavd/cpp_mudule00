67d180b4b401db35b9e0cd75aadd3e6bbf9a7bb4
tavd <mlsuperiv@gmail.com>
2024-10-31 14:54:03 +0100

    Modify megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,10 +2,8 @@
 
 int main(int argc, char **argv)
 {
-	    if (argc == 1) {
-       	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return 0;
-    }
+	    if (argc == 1)
+       	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     for (int i = 1; i < argc; i++) 
 	{
         for (int j = 0; argv[i][j]; j++)
