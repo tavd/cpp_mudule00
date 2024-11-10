f31291835bbeedb7fa624ed6a2812c5c5e0e9949
root <root@DESKTOP-TCA2JI2.localdomain>
2024-11-10 23:02:14 +0100

    Modify some files

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <ctime>     //for std::time and std::localtime
 #include "Account.hpp"
 
 //Even though they are initialized to zero, you typically want to explicitly initialize static members for clarity and control.
@@ -45,7 +46,8 @@ int Account::getNbWithdrawals(void)
 	return _totalNbWithdrawals;
 }
 // Within the class scope: When you are inside a static member function, like Account::getNbAccounts(), 
-// you are already in the class scope. This means the compiler knows that _nbAccounts refers to the static member of the Account class.
+// you are already in the class scope. This means the compiler knows that _nbAccounts refers 
+// to the static member of the Account class.
 // Access to static fields: Since both the static function (getNbAccounts()) and the static field (_nbAccounts) 
 // belong to the same class, there’s no need to qualify _nbAccounts with the class name inside the function.
 int Account::checkAmount(void) const
@@ -55,24 +57,19 @@ int Account::checkAmount(void) const
 
 void Account::_displayTimestamp(void)
 {
-	// time_t is a data type in C/C++ used to represent calendar time.
-	// It's typically the number of seconds that have passed since the Unix epoch (January 1, 1970, 00:00:00 UTC).
+// returns the current time as a time_t value, which is typically the number of seconds that 
+// have elapsed since the Unix epoch (January 1, 1970, 00:00:00 UTC).
 	time_t current_time = std::time(NULL);
 	//struct tm is a structure that holds the components of calendar time, such as year, month, day, hour, minute, and second.
-	// std::localtime(&now) converts the time_t value into a tm structure, which represents the local time. 
+	// std::localtime(&current_time) converts the time_t value into a tm structure, which represents the local time. 
 	// This allows you to break down the time_t value into human-readable components like year, month, day, etc.
 	struct tm timenow = *std::localtime(&current_time);
-	std::cout<<"["<<timenow.tm_year + 1900
-	<<std::setfill('0')<<std::setw(2)
-	<<timenow.tm_mon + 1
-	<<std::setfill('0')<<std::setw(2)
-	<<timenow.tm_mday<<"_"
-	<<std::setfill('0')<<std::setw(2)
-	<<timenow.tm_hour
-	<<std::setfill('0')<<std::setw(2)
-	<<timenow.tm_min
-	<<std::setfill('0')<<std::setw(2)
-	<<timenow.tm_sec<<"] ";
+	std::cout << "[" << timenow.tm_year + 1900
+	<< std::setfill('0') << std::setw(2) << timenow.tm_mon + 1
+	<< std::setfill('0') << std::setw(2) << timenow.tm_mday <<"_"
+	<< std::setfill('0') << std::setw(2) << timenow.tm_hour
+	<< std::setfill('0') << std::setw(2) << timenow.tm_min
+	<< std::setfill('0') << std::setw(2) << timenow.tm_sec << "] ";
 }
 
 //[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
