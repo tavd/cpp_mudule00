24d478988504de1ec86d60e317f91cf62a7a9f3b
Teona Avdiieva <tavdiiev@f0r3s6.codam.nl>
2024-12-01 18:53:54 +0100

    modified ex 02

--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -22,8 +22,9 @@ int		main( void ) {
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
-	size_t const			amounts_size(sizeof(amounts) / sizeof(int));//8 number of elements
+	size_t const			amounts_size(sizeof(amounts) / sizeof(int));//8, number of elements
 	//declares a std::vector<Account> variable named accounts using the range constructor.
+	//creates Account objects and inits them with the elements from amounts
 	accounts_t				accounts(amounts, amounts + amounts_size);
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
